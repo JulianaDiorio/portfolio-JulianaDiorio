d0ba0126ad67c26973fae5ab36a1d69d3b306ec2
Juliana Tissiani Diorio <julianatdiorio@gmail.com>
2023-03-31 09:17:37 -0300

    fix: userData

--- src/utils/userData.ts
+++ src/utils/userData.ts
@@ -1,9 +1,9 @@
 export const userData = {
-  nameUser: "Samuel Leão",
-  githubUser: "samuelleaokenzie",
-  linkedinUser: "nekelpatrick",
-  whatsappNumber: "+5541988819028",
-  emailUser: "samuel@kenzie.com.br",
-  instagramUser: "samuelleaoui",
+  nameUser: "Juliana Tissiani Diorio",
+  githubUser: "JulianaDiorio",
+  linkedinUser: "juliana-tissiani-diorio-4b1553165/",
+  whatsappNumber: "+554198537-9420",
+  emailUser: "julianatdiorio@gmail.com",
+  instagramUser: "juliana.diorio",
   facebookUser: "",
 };
